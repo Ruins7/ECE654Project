7857cdc1a9f363d9a92621a7bc8468ebe8da06c2
Ruins7 <704657512@qq.com>
2016-06-22 15:55:44 -0400

    add demos for try-with-resource and log

--- src/securityAndResources/TestSecurityAndResources02.java
+++ src/securityAndResources/TestSecurityAndResources02.java
@@ -2,30 +2,85 @@
  * @Title: TestSecurityAndResources02.java
  * @Prject: ECE654Project
  * @Package: securityAndResources
- * @Description: TODO
+ * @Description: ECE 654 Project security and resources demo 02 
  * @author: ruins7  
  * @date: Jun 21, 2016 4:22:32 PM
  * @version: V1.0  
  */
 package securityAndResources;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * @ClassName: TestSecurityAndResources02
- * @Description: TODO
+ * @Description: close resources in finally and try-with-resources
  * @author: ruins7
  * @date: Jun 21, 2016 4:22:32 PM
  */
 public class TestSecurityAndResources02 {
 
+	
+	/**
+	 * @Title: cloResWithTWR
+	 * @Description: use try-with-resource 
+	 * @param null
+	 * @return: void
+	 * @throws: FileNotFoundException, IOException
+	 */
+	public static void cloResWithTWR() throws FileNotFoundException, IOException{
+		 
+        try(BufferedReader br = new BufferedReader(new FileReader("/Users/ruins7/Desktop/1234.txt"))){
+            System.out.println("123123123");     
+          	//throw new IOException();
+        } catch(Exception e) {
+        	   System.err.println("error: " + e.getSuppressed().length);
+        	   e.printStackTrace();
+        	   Throwable[] supressed = e.getSuppressed();
+        	   for (int i = 0; i < supressed.length; i++) {
+				Throwable throwable = supressed[i];
+				System.out.println("supressed exception: " + throwable.toString());
+			}
+        }
+	}
+	
+	/**
+	 * @Title: cloResWithFinally
+	 * @Description: use finally 
+	 * both try and finally throw exceptions when use finally, 
+	 * only throw exception in finally,exception in try will be suppressed.  
+	 * @param null
+	 * @return: void
+	 * @throws: FileNotFoundException, IOException
+	 */
+	public static void cloResWithFinally() throws FileNotFoundException, IOException{
+		File file = new File("/Users/ruins7/desktop/1234.txt");
+	    FileInputStream fis = null;
+	    try {//fileNotFound
+			fis = new FileInputStream(file);
+			//other operation
+		} finally {//IOEXception
+			if(fis != null){
+				fis.close();
+			}
+		    throw new IOException();
+		}
+	}
+	
 	/**
 	 * @Title: main
-	 * @Description: TODO
+	 * @Description: main method
 	 * @param args
 	 * @return: void
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		//cloResWithFinally();
+		cloResWithTWR();
 	}
 
 }
---
+++ src/securityAndResources/TestSecurityAndResources03.java
@@ -0,0 +1,85 @@
+/**  
+ * @Title: TestSecurityAndResoureces03.java
+ * @Prject: ECE654Project
+ * @Package: securityAndResources
+ * @Description: TODO
+ * @author: ruins7  
+ * @date: Jun 22, 2016 3:29:00 PM
+ * @version: V1.0  
+ */
+package securityAndResources;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * @ClassName: TestSecurityAndResoureces03
+ * @Description: dome prevent exceptions while logging.
+ * @author: ruins7
+ * @date: Jun 22, 2016 3:29:00 PM
+ */
+public class TestSecurityAndResources03 {
+
+	private static Logger logger = LogManager.getLogger(TestSecurityAndResources03.class.getName());
+	
+	/**
+	 * @Title: standardError
+	 * @Description: prevent use standard error stream to print error information.
+	 * @param null
+	 * @return: void
+	 */
+	public void standardError(){
+		try {
+			throw new IOException();
+		} catch (Exception e) {
+			 System.err.println(e);
+			 System.out.println(e);
+			 e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * @Title: loggerException
+	 * @Description: demo it is possible to lose exception information when another exception occurs while logging.
+	 * @param null
+	 * @return: void
+	 */
+	public void loggerException(){
+		try {
+			throw new IOException();
+		} catch (Exception e) {
+			//simulate there are 10 exceptions messages need to be written in log.
+			for (int i = 0; i < 10; i++) {
+				 logger.debug(e.getStackTrace());
+				 //while logging, a new exception occurs and it interrupts the logging
+				 //in this case, we may lost security exception messages.
+				 if(i == 5){
+					try {
+						throw new FileNotFoundException();
+					} catch (IOException e1) { 
+						System.exit(1);
+					}
+				 }
+			}
+			
+		}
+	}
+	
+	/**
+	 * @Title: main
+	 * @Description: main method
+	 * @param args
+	 * @return: void
+	 */
+	public static void main(String[] args) {
+		TestSecurityAndResources03 tsar = new TestSecurityAndResources03();
+			//	tsar.standardError();
+				tsar.loggerException();
+		
+	}
+
+}
